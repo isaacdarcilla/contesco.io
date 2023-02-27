fdb050954a01b834d110c0ea0a1f8b3632f36d2d
アイザック <isaacdarcilla@gmail.com>
2023-02-27 11:13:11 +0800

    ref: packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import { createClient } from "@supabase/supabase-js";
-import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import { Card } from "flowbite-react";
 import { Lora } from "@next/font/google";
 
 const lora = Lora({ subsets: ["latin"] });
-
-const supabase = createClient(
-  `${process.env.NEXT_PUBLIC_SUPABASE_URL}`,
-  `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
-);
 
 export default function LoginPage() {
   return (
