3af18c3fc73d87db8ee48469301c0965039e935d
Isaac A <isaacdarcilla@gmail.com>
2023-04-21 13:17:14 +0800

    chore(ui): change skeleton color

--- components/loading/Skeleton.tsx
+++ components/loading/Skeleton.tsx
@@ -5,18 +5,18 @@ export default function Skeleton({ center = false }: { center?: boolean }) {
     >
       <div className="w-full text-center rounded-md sm:p-8">
         <div role="status" className="w-full animate-pulse">
-          <div className="h-2.5 bg-gray-500 rounded-full w-48 mb-4"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[360px] mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[330px] mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[300px] mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[360px] mb-4"></div>
-          <div className="h-2.5 bg-gray-500 rounded-full w-48 mb-4"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[360px] mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[330px] mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[300px] mb-2.5"></div>
-          <div className="h-2 bg-gray-500 rounded-full max-w-[360px]"></div>
+          <div className="h-2.5 bg-secondary rounded-full w-48 mb-4"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[360px] mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[330px] mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[300px] mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[360px] mb-4"></div>
+          <div className="h-2.5 bg-secondary rounded-full w-48 mb-4"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[360px] mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[330px] mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[300px] mb-2.5"></div>
+          <div className="h-2 bg-secondary rounded-full max-w-[360px]"></div>
           <span className="sr-only">Loading...</span>
         </div>
       </div>
