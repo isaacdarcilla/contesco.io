208a5ffa502b160fa0fadd7002676e8933cdc4b3
Isaac A <isaacdarcilla@gmail.com>
2023-04-21 11:40:54 +0800

    chore(ui): added breadcrumb

--- app/dashboard/event/[slug]/page.tsx
+++ app/dashboard/event/[slug]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import CustomToast from "@/components/toast/CustomToast";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import Link from "next/link";
 
 type Props = {
   params: {
@@ -15,6 +17,17 @@ export default function EventPage({ params }: Props) {
       <CustomToast />
       <div className="w-full h-full flex flex-col container">
         <section className="container p-12 mx-auto">
+          <Breadcrumb color="gray.200" fontWeight="bold" fontSize="xs">
+            <BreadcrumbItem>
+              <Link href="dashboard">
+                <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+              </Link>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem color="gray">
+              <BreadcrumbLink>Event</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
           <p className="text-xl font-bold text-white">Event, {params.slug}!</p>
         </section>
       </div>
