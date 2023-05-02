7871cdb63509b73003a38c4af48dd8db2fadb949
Isaac A <isaacdarcilla@gmail.com>
2023-05-02 21:12:52 +0800

    chore(ui): animation in logo

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -6,13 +6,26 @@ import { signOut, useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Image from "next/image";
+import { useAnimate } from "framer-motion";
 
 export default function Sidebar({ children }: { children?: React.ReactNode }) {
   const router = useRouter();
   const pathname = usePathname();
   const { status } = useSession();
+  const [scope, animate] = useAnimate();
 
   useEffect(() => {
+    animate(scope.current, {
+      scale: 1.1,
+      rotate: 360,
+      ease: "linear",
+      transition: {
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      },
+    });
+
     if (status == "unauthenticated") {
       router.push("/");
     }
@@ -27,7 +40,13 @@ export default function Sidebar({ children }: { children?: React.ReactNode }) {
     <>
       <aside className="flex flex-col items-center w-14 overflow-hidden text-gray-400 bg-secondary h-screen border-r border-primary">
         <div className="flex items-center justify-center w-10 h-10 mt-1 rounded hover:text-gray-300">
-          <Image src="/logo_96px.png" alt="Logo" width={25} height={25} />{" "}
+          <Image
+            ref={scope}
+            src="/logo_96px.png"
+            alt="Logo"
+            width={25}
+            height={25}
+          />{" "}
         </div>
         <div className="flex flex-col items-center mt-0">{children}</div>
         <div className="flex flex-col items-center mt-0 absolute bottom-2">
@@ -40,6 +59,7 @@ export default function Sidebar({ children }: { children?: React.ReactNode }) {
             >
               <div
                 role="button"
+                title={" "}
                 className="flex items-center justify-center w-10 h-10 mt-2 rounded hover:bg-primary hover:text-gray-300"
               >
                 <Settings role="button" size={18} className="text-white" />
@@ -56,6 +76,7 @@ export default function Sidebar({ children }: { children?: React.ReactNode }) {
           >
             <div
               role="button"
+              title={" "}
               className="flex items-center justify-center w-10 h-10 mt-2 rounded hover:bg-primary hover:text-gray-300"
             >
               <LogOut
